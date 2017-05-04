694814f2997e76fd62e53e58ea61b05e54643534
danny allegrezza <danny@dannyallegrezza.com>
2017-05-03 23:25:02 -0400

    Created PostTag (pure join table)
    Represents a pure join table between the `Post` and `Tag` types

---
+++ dannyallegrezza.DAL/Models/PostTag.cs
@@ -0,0 +1,14 @@
+﻿namespace dannyallegrezza.DAL.Models
+{
+    /// <summary>
+    /// Represents a pure join table between the Post table and the Tag table
+    /// </summary>
+    public class PostTag
+    {
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
