08e17fbb733e0e3f7e685468985f81494e65d552
danny allegrezza <danny@dannyallegrezza.com>
2017-05-03 23:24:06 -0400

    Create Author class
    Used to represent an Author for a piece of content. Inherits from the
    IdentityUser class.

---
+++ dannyallegrezza.DAL/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace dannyallegrezza.DAL.Models
+{
+    public class Author : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private string _fullName;
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+            set { _fullName = value; }
+        }
+
+    }
+}
